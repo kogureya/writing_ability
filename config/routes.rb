648f5c9ec7d71fb46fdd2543eef83e0278f3be39
kogureya <gurelio5908@gmail.com>
2021-04-20 18:06:36 +0900

    詳細表示機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'diaries#index'
 
-  resources :diaries, only: [:new, :create]
+  resources :diaries, only: [:new, :create, :show]
 end
