031303fdc10a7ac5baf3289a3d30f2fa75fb7d75
kogureya <gurelio5908@gmail.com>
2021-05-03 15:14:13 +0900

    使い方説明ページの追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'diaries#index'
 
-  resources :diaries, only: [:new, :create, :show]
+  resources :diaries, only: [:index, :new, :create, :show] do
+    collection do
+      get 'how_to_use'
+    end
+  end
+
   resources :points, only: :index
   resources :feeds, only: [:new, :create]
 end
